edabe3bd17a0187e87a00f5aa146bd3329bdc919
robmikh <rob.mikh@outlook.com>
2024-12-02 22:05:46 -0800

    refactor error

---
+++ src/error.rs
@@ -0,0 +1,53 @@
+use sdl3_sys::error::SDL_GetError;
+
+pub type SdlResult<T> = std::result::Result<T, SdlError>;
+
+#[derive(Debug)]
+pub struct SdlError {
+    pub message: String,
+}
+
+impl std::error::Error for SdlError {}
+
+impl std::fmt::Display for SdlError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl SdlError {
+    fn get_current() -> Self {
+        let message = unsafe {
+            let error_msg_start = SDL_GetError();
+            let mut error_msg_end = error_msg_start;
+            if error_msg_start != std::ptr::null() {
+                while *error_msg_end != 0 {
+                    error_msg_end = error_msg_end.add(1);
+                }
+            }
+            let len = error_msg_end.offset_from(error_msg_start);
+            let error_msg_slice = std::slice::from_raw_parts(error_msg_start as *const u8, len as usize);
+            match std::str::from_utf8(error_msg_slice) {
+                Ok(msg) => format!("SDL Error: {}", msg.to_owned()),
+                Err(error) => format!("Utf8Error: {}", error),
+            }
+        };
+        Self {
+            message,
+        }
+    }
+}
+
+pub trait SdlFunctionResult {
+    fn ok(self) -> SdlResult<()>;
+}
+
+impl SdlFunctionResult for std::primitive::bool {
+    fn ok(self) -> SdlResult<()> {
+        if self {
+            Ok(())
+        } else {
+            Err(SdlError::get_current())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use core::str;
+mod error;
 
-use sdl3_sys::{error::SDL_GetError, init::{SDL_Init, SDL_INIT_VIDEO}};
+use error::{SdlFunctionResult, SdlResult};
+use sdl3_sys::init::{SDL_Init, SDL_INIT_VIDEO};
 
 const SCREEN_WIDTH: i32 = 640;
 const SCREEN_HEIGHT: i32 = 480;
@@ -8,53 +9,14 @@ const SCREEN_HEIGHT: i32 = 480;
 fn main() -> SdlResult<()> {
     init()?;
 
+    
+
     Ok(())
 }
 
-type SdlResult<T> = std::result::Result<T, SdlError>;
-
-#[derive(Debug)]
-struct SdlError {
-    message: String,
-}
-
-impl std::error::Error for SdlError {}
-
-impl std::fmt::Display for SdlError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl SdlError {
-    fn get_current() -> Self {
-        let message = unsafe {
-            let error_msg_start = SDL_GetError();
-            let mut error_msg_end = error_msg_start;
-            if error_msg_start != std::ptr::null() {
-                while *error_msg_end != 0 {
-                    error_msg_end = error_msg_end.add(1);
-                }
-            }
-            let len = error_msg_end.offset_from(error_msg_start);
-            let error_msg_slice = std::slice::from_raw_parts(error_msg_start as *const u8, len as usize);
-            match str::from_utf8(error_msg_slice) {
-                Ok(msg) => format!("SDL Error: {}", msg.to_owned()),
-                Err(error) => format!("Utf8Error: {}", error),
-            }
-        };
-        Self {
-            message,
-        }
-    }
-}
-
 fn init() -> SdlResult<()> {
-    let init_result = unsafe {
-        SDL_Init(SDL_INIT_VIDEO)
-    };
-    if !init_result {
-        return Err(SdlError::get_current());
+    unsafe {
+        SDL_Init(SDL_INIT_VIDEO).ok()?;
     }
     Ok(())
 }
